bc6df47b62950ad38266766dcddb1089442e07db
lakshy1 <lakshyasehgal01@gmail.com>
2024-07-10 17:33:22 +0530

    Uploading Project as per the prerequisites

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex flex-col w-[100vw] h-[100vh] bg-black">
+      <div className="bg-slate-600  w-96 h-96">
+        <p>HI</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
